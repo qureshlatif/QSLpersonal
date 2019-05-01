0c9940559170bf2b68cad5bc6c091c1f84616eb2
Quresh Latif <qureshlatif@yahoo.com>
2019-05-01 17:04:57 -0600

    added tssr function
    Added function written by Adam Green that calculates time since sunrise.

---
+++ R/tssr.R
@@ -0,0 +1,15 @@
+tssr <- function(lat,long,dateTime) {
+  require(suncalc)
+  require(lutz)
+  require(sf)
+  
+  # lat and long as decimal degrees with negative value as southern and western
+  # date as a character e.g., '2009-04-29'
+  # time as a character e.g., '2009-04-29 06:30:00'
+  tz <- tz_lookup_coords(lat=lat,lon=long,method='accurate',warn=FALSE)
+  date <- substr(dateTime, 1, 10)
+  srt <- getSunlightTimes(date=as.Date(date,tz=tz),lat=lat,lon=long,keep='sunrise',tz=tz)$sunrise
+  
+  tssr <- difftime(srt,as.POSIXlt(time.i,tz=tz),units = 'mins')
+  return(tssr=tssr)
+}
