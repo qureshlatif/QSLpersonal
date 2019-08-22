146239769a364cd9cf073d6102e590539a9d82e3
Quresh Latif <qureshlatif@yahoo.com>
2019-08-22 12:42:16 -0600

    debug tssr

--- R/tssr.R
+++ R/tssr.R
@@ -1,15 +1,24 @@
-tssr <- function(lat,long,dateTime) {
+function (lat, long, dateTime) {
   require(suncalc)
   require(lutz)
   require(sf)
-  
   # lat and long as decimal degrees with negative value as southern and western
   # date as a character e.g., '2009-04-29'
   # time as a character e.g., '2009-04-29 06:30:00'
-  tz <- tz_lookup_coords(lat=lat,lon=long,method='accurate',warn=FALSE)
+
+  tz <- tz_lookup_coords(lat = lat, lon = long, method = "accurate", 
+                         warn = FALSE)
   date <- substr(dateTime, 1, 10)
-  srt <- getSunlightTimes(date=as.Date(date,tz=tz),lat=lat,lon=long,keep='sunrise',tz=tz)$sunrise
-  
-  tssr <- as.numeric(difftime(as.POSIXlt(dateTime,tz=tz), srt, units = 'mins'))
-  return(tssr=tssr)
+  tz.unq <- unique(tz)
+  tssr <- rep(NA, length(dateTime))
+  for(z in tz.unq) {
+    ind <- which(tz == z & !is.na(date))
+    dat <- data.frame(lat = lat[ind],
+                      lon = long[ind],
+                      date = as.Date(date[ind], tz = z))
+    srt <- getSunlightTimes(data = dat, keep = "sunrise", tz = z)$sunrise
+    tssr[ind] <- as.numeric(difftime(as.POSIXlt(dateTime[ind], tz = z), 
+                                     srt, units = "mins"))
+  }
+  return(tssr = tssr)
 }
