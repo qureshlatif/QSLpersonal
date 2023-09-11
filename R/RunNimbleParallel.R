acabf3aa00460bc54ccbc7874466a6d52dc04bbe
qureshlatif <qureshlatif@yahoo.com>
2023-09-11 11:35:51 -0600

    various updates RunNimbleParallel
    -Add options for fuzzy tracking of Rhat for specified parameters, setting parameters that should not be ignored for assessing convergence, constraining when and how long to keep running. -Also now writing key objects to global environment to solve errors encountered in some analyses.

--- R/RunNimbleParallel.R
+++ R/RunNimbleParallel.R
@@ -1,8 +1,11 @@
 RunNimbleParallel <-
-  function(model, inits, data, constants, parameters, par.ignore.Rht = c(),
+  function(model, inits, data, constants, parameters,
+           par.ignore.Rht = c(), par.dontign.Rht = c(),
+           par.fuzzy.track.Rht = c(), fuzzy.Rht.threshold = 0.05,
            nc = 2, ni = 2000, nb = 0.5, nt = 10, mod.nam = "mod",
            max.samples.saved = 10000, rtrn.model = F, sav.model = T,
-           Rht.required = 1.1, neff.required = 100) {
+           Rht.required = 1.1, neff.required = 100, check.progress = NULL,
+           max.tries = NULL) {
     if(nb < 1 & (ni - (ni * nb)) < 100) stop("Increase iterations (ni) or reduce burn-in. Too few samples for calculating Rhat.")
     if(nb >= 1 & (ni - nb) < 100) stop("Increase iterations (ni) or reduce burn-in. Too few samples for calculating Rhat.")
     
@@ -12,18 +15,30 @@ RunNimbleParallel <-
     require(mcmcOutput)
     set.seed(1)
     cl<-makeCluster(nc, timeout = 5184000)
-    clusterExport(cl, c("model", "inits", "data", "constants", "parameters",
+    model <<- model
+    inits <<- inits
+    #data <<- data
+    data.inp <<- data
+    constants <<- constants
+    parameters <<- parameters
+    ni <<- ni
+    nt <<- nt
+    clusterExport(cl, c("model", "inits", "data.inp", "constants", "parameters",
                         "ni", "nt"))
     for (j in seq_along(cl)) {
       set.seed(j)
-      init <<- inits()
+      if(is.function(inits)) {
+        init <<- inits()
+      } else {
+        init <<- inits
+      }
       clusterExport(cl[j], "init")
     }
     out1 <- clusterEvalQ(cl, {
       library(nimble)
       library(coda)
       model <- nimbleModel(code = model, name = "model",
-                           constants = constants, data = data,
+                           constants = constants, data = data.inp,
                            inits = init)
       Cmodel <- compileNimble(model)
       modelConf <- configureMCMC(model, thin = nt)
@@ -37,6 +52,7 @@ RunNimbleParallel <-
       CmodelMCMC <- compileNimble(modelMCMC, project = model)
       CmodelMCMC$run(ni, reset = FALSE)
       return(as.mcmc(as.matrix(CmodelMCMC$mvSamples)))
+      gc(verbose = FALSE)
     })
     for(chn in 1:nc) { # nc must be > 1
       ind.keep <- c()
@@ -67,21 +83,40 @@ RunNimbleParallel <-
       select(Parameter, mean:f)
     
     if(length(par.ignore.Rht) == 0) {
-      mxRht <- sumTab %>% pull(Rhat) %>% max(na.rm = T)
-      mn.neff <- sumTab %>% pull(n.eff) %>% min(na.rm = T)
+      mxRht <- sumTab %>% pull(Rhat) %>% max() # na.rm = T
+      mn.neff <- sumTab %>% pull(n.eff) %>% min() # na.rm = T
     } else {
-      ind.ignore <- c()
-      for(p in 1:length(par.ignore.Rht)) ind.ignore <-
-          c(ind.ignore, which(str_detect(sumTab$Parameter, par.ignore.Rht[p]))) %>%
-          unique()
-      if(length(ind.ignore) > 0) {
-        mxRht <- sumTab %>% slice(-ind.ignore) %>% pull(Rhat) %>% max(na.rm = T)
-        mn.neff <- sumTab %>% slice(-ind.ignore) %>% pull(n.eff) %>% min(na.rm = T)
+      if(length(par.dontign.Rht) == 0) {
+        ind.ignore <- which(str_detect_any(sumTab$Parameter, par.ignore.Rht))
       } else {
-        mxRht <- sumTab %>% pull(Rhat) %>% max(na.rm = T)
-        mn.neff <- sumTab %>% pull(n.eff) %>% min(na.rm = T)
+        ind.ignore <- which(str_detect_any(sumTab$Parameter, par.ignore.Rht) &
+                              !str_detect_any(sumTab$Parameter, par.dontign.Rht))
+      }
+      if(length(ind.ignore) > 0) {
+        mxRht <- sumTab %>% slice(-ind.ignore) %>% pull(Rhat) %>% max() # na.rm = T
+        mn.neff <- sumTab %>% slice(-ind.ignore) %>% pull(n.eff) %>% min() # na.rm = T
+      } else {
+        mxRht <- sumTab %>% pull(Rhat) %>% max() # na.rm = T
+        mn.neff <- sumTab %>% pull(n.eff) %>% min() # na.rm = T
       }
     }
+    if(is.na(mxRht) | is.na(mn.neff)) {
+      write.csv(sumTab, "Model_summary.csv")
+      stop("Error: One or more parameters is not being sampled. Check data, initial values, etc., and try again. See 'Model_summary.csv' for parameters missing Rhat or n.eff.")
+    }
+    Rht.fuzzy <- 1 # Putting in at least one value to avoid error later....
+    if(length(par.fuzzy.track.Rht) > 0) {
+      for(p in 1:length(par.fuzzy.track.Rht)) {
+        pfuz <- par.fuzzy.track.Rht[p]
+        Rht.fuzzy <- c(Rht.fuzzy,
+                       sumTab %>%
+                         filter(str_sub(Parameter, 1, nchar(pfuz) + 1) ==
+                                  str_c(pfuz, "[")) %>%
+                         pull(Rhat))
+      }
+      rm(p, pfuz)
+    }
+    if(!is.null(check.progress)) mxRht.1 <- mxRht
     
     mcmc.info <- c(nchains = nc, niterations = ni,
                    burnin = ifelse(nb<1, nb*ni, nb), nthin = nt)
@@ -89,11 +124,21 @@ RunNimbleParallel <-
     if(sav.model) R.utils::saveObject(mod, mod.nam) # If running all in one.
     
     ## If has not converged, continue sampling
-    if(round(mxRht, digits = 1) > Rht.required | mn.neff < neff.required) {
+    if(round(mxRht, digits = 1) > Rht.required |
+       mn.neff < neff.required |
+       (length(Rht.fuzzy) > 0 &
+       ((sum(round(Rht.fuzzy, digits = 1) > Rht.required, na.rm = T) - 1) >
+       ((length(Rht.fuzzy) - 1) * fuzzy.Rht.threshold)))) { # Disregarding the 1 included earlier to avoid an error in round()
       n.runs <- 1
       R.utils::saveObject(out1, str_c(mod.nam, "_chunk", n.runs)) # Save samples from previous run to drive.
     }
-    while(round(mxRht, digits = 1) > Rht.required | mn.neff < neff.required) {
+    rm(mod, sumTab, out1, out2, out.mcmc)
+    gc(verbose = F)
+    while(round(mxRht, digits = 1) > Rht.required |
+          mn.neff < neff.required |
+          (length(Rht.fuzzy) > 0 &
+           ((sum(round(Rht.fuzzy, digits = 1) > Rht.required, na.rm = T) - 1) >
+            ((length(Rht.fuzzy) - 1) * fuzzy.Rht.threshold)))) { # Disregarding the 1 included earlier to avoid an error in round()
       n.runs <- n.runs + 1
       print(str_c("Run = ", n.runs, ". Max Rhat = ", mxRht, " and min neff = ", mn.neff,
                   ". Keep on chugging."))
@@ -157,25 +202,68 @@ RunNimbleParallel <-
         mutate(Parameter = row.names(sumTab)) %>%
         select(Parameter, mean:f)
       if(length(par.ignore.Rht) == 0) {
-        mxRht <- sumTab  %>% pull(Rhat) %>% max(na.rm = T)
-        mn.neff <- sumTab %>% pull(n.eff) %>% min(na.rm = T)
+        mxRht <- sumTab %>% pull(Rhat) %>% max() # na.rm = T
+        mn.neff <- sumTab %>% pull(n.eff) %>% min() # na.rm = T
       } else {
-        if(length(ind.ignore) > 0) {
-          mxRht <- sumTab %>% slice(-ind.ignore) %>% pull(Rhat) %>% max(na.rm = T)
-          mn.neff <- sumTab %>% slice(-ind.ignore) %>% pull(n.eff) %>% min(na.rm = T)
+        if(length(par.dontign.Rht) == 0) {
+          ind.ignore <- which(str_detect_any(sumTab$Parameter, par.ignore.Rht))
         } else {
-          mxRht <- sumTab %>% pull(Rhat) %>% max(na.rm = T)
-          mn.neff <- sumTab %>% pull(n.eff) %>% min(na.rm = T)
+          ind.ignore <- which(str_detect_any(sumTab$Parameter, par.ignore.Rht) &
+                                !str_detect_any(sumTab$Parameter, par.dontign.Rht))
+        }
+        if(length(ind.ignore) > 0) {
+          mxRht <- sumTab %>% slice(-ind.ignore) %>% pull(Rhat) %>% max() # na.rm = T
+          mn.neff <- sumTab %>% slice(-ind.ignore) %>% pull(n.eff) %>% min() # na.rm = T
+        } else {
+          mxRht <- sumTab %>% pull(Rhat) %>% max() # na.rm = T
+          mn.neff <- sumTab %>% pull(n.eff) %>% min() # na.rm = T
         }
       }
-      gc(verbose = F)
+      Rht.fuzzy <- 1 # Putting in at least one value to avoid error later....
+      if(length(par.fuzzy.track.Rht) > 0) {
+        for(p in 1:length(par.fuzzy.track.Rht)) {
+          pfuz <- par.fuzzy.track.Rht[p]
+          Rht.fuzzy <- c(Rht.fuzzy,
+                         sumTab %>%
+                           filter(str_sub(Parameter, 1, nchar(pfuz) + 1) ==
+                                    str_c(pfuz, "[")) %>%
+                           pull(Rhat))
+        }
+        rm(p, pfuz)
+      }
+      gc(verbose = FALSE)
       
       mcmc.info <- c(nchains = nc, niterations = ni * n.runs,
                      burnin = ifelse(nb<1, round(ni.saved * nb), nb),
                      nthin = nt * nt2)
+      if(!is.null(check.progress)) {
+        if(n.runs == check.progress & mxRht >= mxRht.1 & mxRht > 2) {
+          warn.message <- str_c("Insufficient progress towards convergence - Rhat has not decreased after ", check.progress,
+                                " runs. Abandoning model. Recommend checking model output and trying a different model.")
+          mod <- list(mcmcOutput = mod, summary = sumTab, mcmc.info = mcmc.info,
+                      warning = warn.message)
+          if(sav.model) R.utils::saveObject(mod, mod.nam)
+          print(warn.message)
+          break
+        }
+      }
+      if(!is.null(max.tries)) {
+        if(n.runs == max.tries) {
+          warn.message <- "Maximum number of runs reached. Abandoning model. Recommend checking model output and trying a different model."
+          mod <- list(mcmcOutput = mod, summary = sumTab, mcmc.info = mcmc.info,
+                      warning = warn.message)
+          if(sav.model) R.utils::saveObject(mod, mod.nam)
+          print(warn.message)
+          break
+        }
+      }
       mod <- list(mcmcOutput = mod, summary = sumTab, mcmc.info = mcmc.info)
       if(sav.model) R.utils::saveObject(mod, mod.nam) # If running all in one.
+      
+      rm(mod, sumTab, out1, out2, out3, out.r, out.mcmc.update)
+      gc(verbose = F)
     }
     if(exists("n.runs")) for(r in 1:n.runs) file.remove(str_c(mod.nam, "_chunk", r))
     if(rtrn.model) return(mod)
+    stopCluster(cl)
   }
